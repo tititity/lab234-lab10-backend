63b27688794db7a45db2b6e09469d39c0ddce683
Unknown <spsr5032@gmail.com>
2018-04-20 00:01:28 +0700

    add GradeServiceTest.java

---
+++ src/test/java/camt/se234/lab10/service/TestGradeService.java
@@ -0,0 +1,23 @@
+package camt.se234.lab10.service;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestGradeService {
+    @Test
+    public void testGetGrade(){
+        GradeServiceImpl gradeService = new GradeServiceImpl();
+        assertThat(gradeService.getGrade(100),is("A"));
+        assertThat(gradeService.getGrade(80),is("A"));
+        assertThat(gradeService.getGrade(78.9),is("B"));
+        assertThat(gradeService.getGrade(75),is("B"));
+        assertThat(gradeService.getGrade(74.4),is("C"));
+        assertThat(gradeService.getGrade(60),is("C"));
+        assertThat(gradeService.getGrade(59.4),is("D"));
+        assertThat(gradeService.getGrade(33),is("D"));
+        assertThat(gradeService.getGrade(32),is("F"));
+        assertThat(gradeService.getGrade(0),is("F"));
+
+    }
+}
