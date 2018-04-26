b3fdd8bcf693c78458b7e1b87fc4d4934071290b
Unknown <spsr5032@gmail.com>
2018-04-26 18:09:05 +0700

    implement test code

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -23,6 +23,18 @@ public class GradeServiceImpl implements GradeService {
 
     @Override
     public String getGrade(double midtermScore, double finalScore) {
-       return null;
+        double score = midtermScore + finalScore;
+        if (score > 79.5) {
+            return "A";
+        }
+        else if (score > 74.5){
+            return "B";
+        }else if (score > 59.5){
+            return "C";
+        }else if (score > 32.5) {
+            return "D";
+        } else
+            return "F";
+
     }
 }
